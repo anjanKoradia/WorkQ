18c7169b4fc8bdf77853d03b46adac22bde935a5
Anjan Koradia <anjankoradia2101@gmail.com>
2022-02-28 18:18:56 +0530

    Use scss instead of css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,3 @@
 let mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js/app.js').css('resources/css/app.css', 'public/css/app.css')
+mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss', 'public/css/app.css')
