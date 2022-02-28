db8e123d980612b75f979b11737821d6b064129a
Anjan Koradia <anjankoradia2101@gmail.com>
2022-02-28 18:00:04 +0530

    Navbar created

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.render("layout")
+})
+
+/* ---------------------------------------- 
+  Assets
+---------------------------------------- */
+app.use(express.static("public"));
+
+/* ---------------------------------------- 
+  Set template engine
+---------------------------------------- */
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
 
 /* ---------------------------------------- 
   Start server
 ---------------------------------------- */
 const server = app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
