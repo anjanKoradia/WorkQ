030570c57fbd8ac1547b124e9eb96323dae94de3
Anjan Koradia <anjankoradia2101@gmail.com>
2022-03-02 10:58:48 +0530

    Database connection established

--- server.js
+++ server.js
@@ -3,7 +3,21 @@ const app = express();
 const ejs = require("ejs");
 const expressLayout = require("express-ejs-layouts");
 const path = require("path");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
+
+/* ---------------------------------------- 
+  Database connection 
+---------------------------------------- */
+const url = "mongodb://localhost/WorkQ";
+mongoose.connect(url).then(
+  () => {
+    console.log("Database connected...");
+  },
+  (err) => {
+    console.log("Connection failed...");
+  }
+);
 
 /* ---------------------------------------- 
   Assets
@@ -18,6 +32,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+
+/* ---------------------------------------- 
+  App Config
+---------------------------------------- */
+global.appRoot = path.resolve(__dirname);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 /* ---------------------------------------- 
   Routes
