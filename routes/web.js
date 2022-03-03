c1626e8420c5cc2cf83253e73882866f3bf704e2
Anjan Koradia <anjankoradia2101@gmail.com>
2022-03-03 19:13:12 +0530

    Add middleware to prevent loged in user to access login/signup page

--- routes/web.js
+++ routes/web.js
@@ -1,18 +1,21 @@
 const loginController = require("../app/http/controllers/auth/loginController");
 const signUpController = require("../app/http/controllers/auth/signUpController")
 
+// Middleware
+const guest = require("../app/http/middleware/guest");
+
 function initRoutes(app) {
 
     //signup
-    app.get("/authentication/signup/step1", signUpController().index1);
+    app.get("/authentication/signup/step1", guest, signUpController().index1);
     app.post("/authentication/signup/step1", signUpController().step1);
     // ---
-    app.get("/authentication/signup/step2", signUpController().index2);
+    app.get("/authentication/signup/step2", guest, signUpController().index2);
     app.post("/authentication/signup/step2", signUpController().step2);
 
 
     // login
-    app.get("/authentication/login", loginController().index);
+    app.get("/authentication/login", guest, loginController().index);
     app.post("/authentication/login", loginController().login);
 
 }
